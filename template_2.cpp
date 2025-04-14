7e38d6cb64aa54980ebfad7e24847ceac96efddb
Swapnil <imswapnil17@gmail.com>
2025-04-14 20:08:27 +0530

    Template used in class

--- template_2.cpp
+++ template_2.cpp
@@ -1,21 +1,39 @@
 #include <conio.h>
 #include <iostream>
 
-class ArrayList{
+template<class X> class ArrayList{
 private:
 	struct ControlBlock{
 		int capacity;
-		int *arr_ptr;
+		X *arr_ptr;
 	};
 	ControlBlock *s;
 public:
 	ArrayList(int capacity){
 		s = new ControlBlock;
 		s->capacity = capacity;
-		s->arr_ptr = new int[s->capacity];
+		s->arr_ptr = new X[s->capacity];
+		s->arr_ptr[capacity] = {0};
 	}
-};
+	void addElement(int index, X data){
+		if(index>=0 &&index<=s->capacity-1){
+			s->arr_ptr[index] = data;
+		}
+		else{
+			std::cout<<"Array index is not valid"<<std::endl;
+		}
+	} 
+	void viewList(){
+		int i; 
+		for(i=0;i<s->capacity;i++){
+			std::cout<<" "<<s->arr_ptr[i];
+		}
+		std::cout<<"\n";
+	}
+};	
 int main(){
-	std::cout<<"Hello world~!"<<std::endl;
+	ArrayList <float> list1(4);
+	list1.addElement(2,3.56);
+	list1.viewList();
 	return 0;
 }
